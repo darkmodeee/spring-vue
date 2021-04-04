e224ba3f9225cee3c7b57466a26aeac443c40bdd
Dark Mode : On <muffintoppings36@gmail.com>
2021-04-04 19:48:15 +0800

    Initial commit

---
+++ backend/src/test/java/com/darkmode/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darkmode.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
