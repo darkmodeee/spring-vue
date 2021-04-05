ca9509bfd8b33201596eacd6565de69a58166adb
Dark Mode : On <muffintoppings36@gmail.com>
2021-04-05 15:58:46 +0800

    Hello rest api

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="hello">hello</button>
+    {{response}}
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  data () {
+    return {
+      response: '',
+      errors: []
+    }
+  },
+  methods: {
+    hello () {
+      axios.get('/api/hello')
+        .then(res => {
+          this.response = res.data
+        }).catch(err => this.errors.push(err))
+    }
   }
 }
 </script>
