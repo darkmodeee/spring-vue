1d2fdc169e122c16cda8d07ce3833ac025b66672
Dark Mode : On <muffintoppings36@gmail.com>
2021-04-05 15:57:08 +0800

    Proxy server starting from /api endpoint

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  // proxy all webpack dev-server requests starting with /rest
+  // proxy all webpack dev-server requests starting with /api
   // to our Spring Boot backend (localhost:8098) using http-proxy-middleware
   // see https://cli.vuejs.org/config/#devserver-proxy
   devServer: {
     proxy: {
-      '/rest': {
+      '/api': {
         target: 'http://localhost:8098', // this configuration needs to correspond to the Spring Boot backends' application.properties server.port
         ws: true,
         changeOrigin: true
