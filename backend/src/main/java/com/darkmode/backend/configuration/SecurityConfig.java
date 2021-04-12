57a62cb07fd7c50ff63b7f525b527464380854a3
Dark Mode : On <muffintoppings36@gmail.com>
2021-04-13 02:47:41 +0800

    Ignore static web resources in spring security

---
+++ backend/src/main/java/com/darkmode/backend/configuration/SecurityConfig.java
@@ -0,0 +1,17 @@
+package com.darkmode.backend.configuration;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+}
