a50a8be0dcc6f6e942f728e8f529f385ee618e72
Dark Mode : On <muffintoppings36@gmail.com>
2021-04-13 20:11:17 +0800

    Enable http security

--- backend/src/main/java/com/darkmode/backend/configuration/SecurityConfig.java
+++ backend/src/main/java/com/darkmode/backend/configuration/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.darkmode.backend.configuration;
 
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,5 +14,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         web
                 .ignoring()
                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(authorize -> authorize
+                        .anyRequest().authenticated()
+                );
     }
 }
